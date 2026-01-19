5c72432e895affb3f403295badfbc5c694f8c7c9
Voldemar <volodya.space@gmail.com>
2026-01-19 15:10:04 +0300

    create views and urls

--- main/admin.py
+++ main/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import CategoryModel, SizeModel, ProductModel, \
+    ProductImageModel, ProductSizeModel
+    
 
-# Register your models here.
+class ProductImageInline(admin.TabularInline):
+    model = ProductImageModel
+    extra = 1
+    
+class ProductSizeInline(admin.TabularInline):
+    model = ProductSizeModel
+    extra = 1
+    
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'color', 'price']
+    list_filter = ['category', 'color']
+    search_fields = ['name', 'color', 'description']
+    prepopulated_fields = {'slug': ('name',)}
+    inlines = [ProductImageInline, ProductSizeInline]
+    
+    
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name',)}
+    
+class SizeAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    
+
+admin.site.register(CategoryModel ,CategoryAdmin)
+admin.site.register(SizeModel ,SizeAdmin)
+admin.site.register(ProductModel ,ProductAdmin)
--- main/views.py
+++ main/views.py
@@ -1,3 +1,133 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.views.generic import TemplateView, DetailView
+from django.http import HttpResponse
+from django.template.response import TemplateResponse
+from django.db.models import Q
 
-# Create your views here.
+from .models import ProductModel
+
+def product_detail(request, product_id):
+    # Получаем товар или 404
+    product = get_object_or_404(ProductModel, id=product_id)
+    
+    return render(request, 'main/product_detail.html', {
+        'product': product
+    })
+
+def product_by_slug(request, slug):
+    # По slug
+    product = get_object_or_404(ProductModel, slug=slug, available=True)
+    
+    return render(request, 'main/product_detail.html', {
+        'product': product
+    })
+from .models import CategoryModel, ProductModel, SizeModel
+
+
+
+class IndexView(TemplateView):
+    template_name = 'main/base.html'
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['categories'] = CategoryModel.objects.all()
+        context['current_category'] = None
+        return context
+    
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        if request.headers.get('HX-Request'):
+            return TemplateResponse(request, 'main/home_context.html', context)    
+        return TemplateResponse(request, self.template_name, context)
+    
+    
+class CatalogView(TemplateView):
+    template = 'main/base.html'
+    FILTER_MAPPING = {
+        'color': lambda queryset, value: queryset.filter(color__iexact=value),
+        'min_price': lambda queryset, value: queryset.filter(price_gte=value),
+        'max_price': lambda queryset, value: queryset.filter(price_lte=value),
+        'size': lambda queryset, value: queryset.filter(product_size__size=value),
+    }
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        category_slug = kwargs.get('category_slug')
+        categories = CategoryModel.objects.all()
+        products = ProductModel.objects.all().order_by('-created_at')
+        current_category = None
+        
+        if category_slug:
+            current_category = get_object_or_404(CategoryModel, slug=category_slug)
+            products = products.filter(category=current_category)
+        query = self.request.GET.get('q')
+        if query:
+            products = products.filter(Q(name__icontains=query)|Q(description__icontains=query))
+        filter_params ={}
+        for param, filter_func in self.FILTER_MAPPING.items():
+            value = self.request.GET.get(param)
+            if value:
+                products = filter_func(products, value)
+                filter_params[param] = value
+            else:
+                filter_params[param] = ''
+        
+        filter_params['q'] = query or ''
+        
+        context.update({
+            'categories': categories,
+            'products': products,
+            'current_category': category_slug,
+            'filter_params': filter_params,
+            'sizes': SizeModel.objects.all(),
+            'search_query': query or ''
+            })
+        
+        if self.request.GET.get('show_search') == 'true':
+            context['show_search'] = True
+        elif self.request.GET.get('reset_search') == 'true':
+            context['reset_search'] = True
+            
+        return context
+    
+    
+    def get(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs) 
+        if request.headers.get('HX-Reequest'):
+            if context.get('show_search'):
+            
+                return TemplateResponse(request, 'main/search_input.html', context)
+            elif context.get('reset_search'):
+                return TemplateResponse(request, 'main/search_button.html', {})     
+            
+            template = 'main/filter_madal.html' if request.GET.get('show_filters') == 'true' else 'main/catalog.html' 
+            return TemplateResponse(request, template, context)
+        
+        return TemplateResponse(request, self.template_name, context)
+    
+    
+    
+class ProductDetailView(DetailView):
+        model = ProductModel
+        template_name = 'main/base.html'
+        slug_field = 'slug'
+        slug_url_kwarg = 'slug'
+        
+        def get_context_data(self, **kwargs):
+            context = super().get_context_data(**kwargs)
+            product = self.get_object()
+            context['categories'] = CategoryModel.objects.all()
+            context['related_products'] = ProductModel.objects.filter(
+                category = product.category
+            ).exclude(id=product.id)[:4]
+            context['current_category'] = product.category.slug
+            return context
+        def get(self, request, *args, **kwargs):
+            self.object = self.get_object()
+            context = self.get_context_data(**kwargs)
+            if request.headers.get('HX-Request'):
+                return TemplateResponse(request, 'nain/product_detail.html', context)
+            
+            raise TemplateResponse(request, self. template_name, context)
+    
+
+        
