a9bf487f600e0752546e92599cbcbaf3069ee2ec
Voldemar <volodya.space@gmail.com>
2026-01-21 17:31:04 +0300

    Исправлен баг db

--- enf/settings.py
+++ enf/settings.py
@@ -9,9 +9,8 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
-from pathlib import Path
 import os
+from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -45,6 +44,7 @@ INSTALLED_APPS = [
     
     'main',
     'cart',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -86,11 +86,14 @@ WSGI_APPLICATION = 'enf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
+        'HOST': os.getenv('POSTGRES_HOST'),
+        'PORT': os.getenv('POSTGRES_PORT'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
@@ -144,3 +147,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_AGE = 86400 #30DAY
 SESSION_SAVE_EVERY_REQUEST = True
+
+
+AUTH_USER_MODEL = 'users.CustomUser'
